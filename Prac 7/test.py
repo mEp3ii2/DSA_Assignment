806d8f906434e77f9f0e9d6f90f7b3bcac4b3fc7
mEp3ii2 <74845700+mEp3ii2@users.noreply.github.com>
2023-05-13 22:48:17 +0800

    Add files via upload
    Added location class to hash functions

--- Prac 7/test.py
+++ Prac 7/test.py
@@ -3,8 +3,5 @@ from DSAHashEntry import *
 import numpy as np
 
 tabs = DSAHashTable(100)
-tabs.put(10,11)
-tabs.put(15,12)
-tabs.put(23,'hejhfb')
-tabs.remove(10)
+tabs.importFile()
 tabs.display()
